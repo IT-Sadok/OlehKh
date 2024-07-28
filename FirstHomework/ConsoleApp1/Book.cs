99944a0a7cdcdca08e86fce78f7786840bfdda6c
OlegioKh <kharchenko.oleg.plt@gmail.com>
2024-07-28 22:55:27 +0300

    New_commit_28/07

---
+++ FirstHomework/ConsoleApp1/Book.cs
@@ -0,0 +1,21 @@
+﻿class Book
+{
+    // Properties specific to a Book
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    // Constructor to initialize Book properties
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    // DisplayBookInfo
+    public void DisplayBookInfo()
+    {
+        Console.WriteLine($"Title: {Title}, Author: {Author}");
+    }
+
+
+}
--- FirstHomework/ConsoleApp1/Program.cs
+++ FirstHomework/ConsoleApp1/Program.cs
@@ -1,21 +1,25 @@
-﻿LibraryCount reader1 = new LibraryCount();
-Messages Mes1 = new Messages();
+﻿CalculationBooksFlow reader1 = new CalculationBooksFlow();
+Logger Log1 = new Logger();
 Validator Fig1 = new Validator();
+Library library1 = new Library();
+Top5BooksToGet Top5 = new Top5BooksToGet();
 
 Console.WriteLine("Welcome to our library! Do you want to take a book or give it back?");
 
-string actionReply = Console.ReadLine();
+string? actionReply = Console.ReadLine();
+
+int number; // for further calculations(number of books)
 
 switch (actionReply)
 {
     case "take":
-        Mes1.PrintTakeQuestion();
-        string booksReply = Console.ReadLine();
-        bool isInt1 = Fig1.TryParseInt(booksReply, out int number);
-        if (isInt1)
+        Log1.PrintTakeQuestion();        ;
+        if (Fig1.TryParseInt(Log1.GetReplyHowManyBooks(), out number))
         {
-            int result = reader1.Remove(number, reader1.Deposit);
-            Mes1.PrintCalculationResult(result);
+            int result = reader1.Remove(number);
+            Log1.GiveListOfBestBooks();
+            Top5.PrintBooks();
+            Log1.PrintCalculationResult(result);
         }
         else
         {
@@ -23,14 +27,21 @@ switch (actionReply)
         }
         break;
 
+
     case "back":
-        Mes1.PrintGiveBackQuestion();
-        string booksReply2 = Console.ReadLine();
-        bool isInt2 = Fig1.TryParseInt(booksReply2, out int number2);
-        if (isInt2)
+        Log1.PrintGiveBackQuestion();
+        if (Fig1.TryParseInt(Log1.GetReplyHowManyBooks(), out number))
         {
-            int result = reader1.Add(number2, reader1.Deposit);
-            Mes1.PrintCalculationResult(result);
+            int result = reader1.Add(number);
+            Log1.GetListOfBooks();
+            library1.AddBooksInfo(new Book("On the Origin of Species", "Charles Darwin"));
+            library1.AddBooksInfo(new Book("The Great Gatsby", "F SCOTT FITZGERALD"));
+            library1.AddBooksInfo(new Book("The Catcher In The Rye", "J.D. SALINGER"));
+            library1.AddBooksInfo(new Book("1984", "GEORGE ORWEL"));
+            library1.AddBooksInfo(new Book("Lord Of The Rings Trilogy", "J.R.R. TOLKIEN"));
+            library1.DisplayBooks();
+
+            Log1.PrintCalculationResult(result);
         }
         else
         {
