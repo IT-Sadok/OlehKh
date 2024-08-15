122bd84bbd4b6f4cbd48bb53e02952d750344f8f
OlegioKh <kharchenko.oleg.plt@gmail.com>
2024-08-15 16:45:55 +0300

    Update

--- SecondHomework/Logistic/Result.cs
+++ SecondHomework/Logistic/Result.cs
@@ -1,12 +1,7 @@
 ﻿public class Result
 {
     public bool Success { get; private set; }
-    public string Message { get; private set; }
-
-    public Result()
-    {
-            
-    }
+    public string? Message { get; private set; }
 
     public void SetResult(bool success, string message)
     {
