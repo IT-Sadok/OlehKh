ed092eb6aaf389dd70c34b3cf3529bf78353e5e4
OlegioKh <kharchenko.oleg.plt@gmail.com>
2024-09-28 09:41:23 +0300

    adding_logic_to_mapper

--- WEB_first project/ASP.NET CORE Project#1/Mappings/MappingProfile.cs
+++ WEB_first project/ASP.NET CORE Project#1/Mappings/MappingProfile.cs
@@ -9,16 +9,14 @@ namespace ASP.NET_CORE_Project_1.Mappings
         public MappingProfile()
         {
             CreateMap<BaseSignUpModel, Account>()
-                .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender))
-                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age))
-                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => "passenger"));
+            .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender))
+            .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age));
 
             CreateMap<DriverSignUpModel, Account>()
                 .ForMember(dest => dest.DrivingExperienceYears, opt => opt.MapFrom(src => src.Experience))
                 .ForMember(dest => dest.CarModel, opt => opt.MapFrom(src => src.CarModel))
                 .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender))
-                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age))
-                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => "driver"));
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age));
         }
     }
 }
